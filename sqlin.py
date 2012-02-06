e09b707154f64f04c296b443e9276302eae3cc80
Jake Billo <jake@jakebillo.com>
2012-02-06 17:07:33 -0500

    Documentation on when and why to use this script

--- sqlin.py
+++ sqlin.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python
 
+"""
+sqlin
+
+In some of the work I do, one of the most frequent tasks is pulling out a numeric
+identifier column and relating it to another table. This is due to poor database
+design that I have nothing to do with, but essentially makes cross-selects
+prohibitive. 
+
+Here's what you do as an example, assuming you have a terrible Drupal site and
+a view that provides an animal type and breed that relate to specific terms.
+
+$ mysql
+mysql> \T /tmp/output.txt
+mysql> select vid from fancy_view where type = 'Dog' and breed = 'Chihuahua';
+   (you'll see a whole bunch of VID's)
+mysql> \! sqlin.py /tmp/output.txt
+('10000', '10001', '10002', '10003')
+
+   (this makes it quite easy to copy and paste an IN statement, for example)
+
+mysql> select adoption_code from dogs where vid in <<<PASTE HERE>>>;
+
+"""
+
 import sys
 
 f = open(sys.argv[1], 'r')
