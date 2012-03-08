22636192cbedefb64e09a04b259a46688b4c2851
Jake Billo <jake@jakebillo.com>
2012-03-08 12:43:13 -0500

    Learn spam script for SlightlySauced or a custom mail directory. Use with crontab and 2&>1 to redirect output.

--- learn_spam.sh
+++ learn_spam.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sa-learn --spam /home/vmail/slightlysauced.com/contact/Maildir/.spam/cur/* && rm -f /home/vmail/slightlysauced.com/contact/Maildir/.spam/cur/*
+DOMAIN=slightlysauced.com
+USERNAME=contact
+
+sa-learn --spam /home/vmail/${DOMAIN}/${USERNAME}/Maildir/.spam/cur/* && rm -f /home/vmail/${DOMAIN}/${USERNAME}/Maildir/.spam/cur/*
