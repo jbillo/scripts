5f1b70f8908a5b54afa758f85642427759464d34
Jake Billo <jake@jakebillo.com>
2017-11-18 18:17:25 -0500

    Create docker-nzbget.sh

---
+++ docker/docker-nzbget.sh
@@ -0,0 +1,9 @@
+docker create \
+    --name nzbget \
+    -p 6789:6789 \
+    -e PUID=1000 -e PGID=1000 \
+    -e TZ="America/Toronto" \
+    -v /mnt/ssd/config/nzbget:/config \
+    -v /scratch/nzbget/downloads:/downloads \
+    --restart unless-stopped \
+    linuxserver/nzbget
