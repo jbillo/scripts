28c82b558062dafdea96047911c050f110b00ce9
Jake Billo <jake@jakebillo.com>
2017-11-18 19:16:22 -0500

    Update docker-radarr.sh

--- docker/docker-radarr.sh
+++ docker/docker-radarr.sh
@@ -1,7 +1,7 @@
 /usr/bin/docker run \
                               -d \
                               --name=radarr \
-                              -v /opt/radarr/config:/config \
+                              -v /mnt/ssd/config/radarr:/config \
                               -v /mnt/drobo5n:/mnt/drobo5n \
                               -v /mnt/5n2:/mnt/5n2 \
                               -e PGID=1000 -e PUID=1000  \
