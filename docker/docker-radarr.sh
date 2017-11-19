0e3b63d34ab95fb382f9df84bdcb106a29756df7
Jake Billo <jake@jakebillo.com>
2017-11-18 19:20:00 -0500

    Update docker-radarr.sh

--- docker/docker-radarr.sh
+++ docker/docker-radarr.sh
@@ -4,6 +4,7 @@
                               -v /mnt/ssd/config/radarr:/config \
                               -v /mnt/drobo5n:/mnt/drobo5n \
                               -v /mnt/5n2:/mnt/5n2 \
+                              -v /mnt/5n2/Downloads/Complete:/downloads \
                               -e PGID=1000 -e PUID=1000  \
                               -e TZ="America/Toronto" \
                               -p 7878:7878 \
